202d749b92ddd96446c375c5cc4826bfa582284d
jbrenorv <joao.breno.ro17@gmail.com>
2022-06-09 11:53:39 -0300

    selecionando uma casa

--- components/Casa.jsx
+++ components/Casa.jsx
@@ -2,15 +2,16 @@ export default function Casa(props) {
   return (
     <div
       style={{
-        backgroundColor: props.cor ?? "#000",
+        backgroundColor: props.cor,
       }}
     >
       <div
         style={{
           height: "100%",
           width: "100%",
-          backgroundColor: "rgba(0,255,0,.5)",
+          backgroundColor: props.selected ? "rgba(0,255,0,.5)" : props.cor,
         }}
+        onClick={props.onClick}
       ></div>
     </div>
   );
--- pages/tabuleiro.jsx
+++ pages/tabuleiro.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Casa from "../components/Casa";
 
 export default function Tabuleiro() {
-  const [index, setIndex] = useState();
+  const [index, setIndex] = useState(-1);
 
   const getColor = (index) => {
     const linha = Math.floor(index / 8);
@@ -12,13 +12,23 @@ export default function Tabuleiro() {
     return "#b78963";
   };
 
+  const selecionarCasa = (idx) => {
+    if (index === idx) setIndex(-1);
+    else setIndex(idx);
+  };
+
   return (
     <div className="tabuleiro-pagina">
       <div className="tabuleiro">
         {Array(64)
           .fill(0)
           .map((_, idx) => (
-            <Casa cor={getColor(idx)} />
+            <Casa
+              cor={getColor(idx)}
+              index={idx}
+              selected={index === idx}
+              onClick={() => selecionarCasa(idx)}
+            />
           ))}
       </div>
     </div>
